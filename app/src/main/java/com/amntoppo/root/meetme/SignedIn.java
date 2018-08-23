45a5aa55ef0da9da2c0795d5a7c64d4c56a5edc1
root <amntoppo@gmail.com>
2018-08-23 09:38:12 +0530

    Added OTP login

---
+++ app/src/main/java/com/amntoppo/root/meetme/SignedIn.java
@@ -0,0 +1,13 @@
+package com.amntoppo.root.meetme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignedIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+    }
+}
